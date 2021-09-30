5f5c401010c0de51c1c5773bfc63e301190ed568
shikharr22 <shikharraizaday@gmail.com>
2021-09-30 20:39:24 +0530

    made changes to UI

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const map = L.map("map").setView([54.526, 15.2551], 4);
 const url =
   "https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=hs3S6M6cLXWe5u0OssHP";
 const attribution =
-  '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>>';
+  '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
 const tileLayer = L.tileLayer(url, { attribution });
 
 tileLayer.addTo(map);
+
 function generateList() {
   let ul = document.querySelector(".tracks");
   tracksData.forEach((track) => {
@@ -14,15 +15,22 @@ function generateList() {
     const div = document.createElement("div");
     const a = document.createElement("a");
     const p = document.createElement("p");
+    const image = document.createElement("img");
     a.addEventListener("click", () => {
       toTrack(track);
+      document.getElementById("list").style.display = "none";
+      document.getElementById("map").style.width = "100%";
     });
     div.classList.add("item");
     p.classList.add("trackName");
     a.href = "#";
     p.innerText = track.properties.name;
+    image.classList.add("track-image");
     a.appendChild(p);
+    a.appendChild(image);
+    image.src = track.properties.thumbnail;
     div.appendChild(a);
+
     li.appendChild(div);
     ul.appendChild(li);
   });
@@ -32,7 +40,7 @@ generateList();
 
 var myMarker = L.icon({
   iconUrl: "./marker.png",
-  iconSize: [60, 80],
+  iconSize: [40, 40],
 });
 
 function popupContent(feature) {
@@ -40,7 +48,6 @@ function popupContent(feature) {
 }
 
 const trackLayer = L.geoJSON(tracksData, {
-  
   pointToLayer: (feature, latlng) => {
     return L.marker(latlng, { icon: myMarker }).on("click", () =>
       toTrack(feature)
@@ -48,29 +55,62 @@ const trackLayer = L.geoJSON(tracksData, {
   },
 });
 
-
-
 trackLayer.addTo(map);
 
-function toTrack(track) {
-  const a=track.geometry.coordinates[1]
-  const b=track.geometry.coordinates[0]
-  map.flyTo(
-    [a,b],
-    15,
-    { duration: 1 }
-  );
-  
-  setTimeout(()=>{
-    document.getElementById('image').innerHTML=`<img src=${track.properties.imageUrl} style="position:absolute;top:32px;right:32px;z-index:10000;
+const toTrack = async (track) => {
+  const a = track.geometry.coordinates[1];
+  const b = track.geometry.coordinates[0];
+
+  map.flyTo([a, b], 15, { duration: 1 });
+
+  setTimeout(() => {
+    document.getElementById(
+      "image"
+    ).innerHTML = `<img src=${track.properties.imageUrl} style="position:absolute;top:32px;right:32px;z-index:10000;
     width:250px;height:400px;border-radius:5px;box-shadow: 0px 5px 8px 1px rgba(255,255,255,0.79);
     -webkit-box-shadow: 0px 5px 8px 1px rgba(255,255,255,0.79);
-    -moz-box-shadow: 0px 5px 8px 1px rgba(255,255,255,0.79);">`
-  },1000);
-  setTimeout(()=>{
-    document.getElementById('video').innerHTML=`<video  controls width=480 height=320 autoplay style='position:absolute;top:32px;left:60px;z-index:10000'><source src='${track.properties.videoUrl}' type='video/mp4'></video>`;
-  },2000);
+    -moz-box-shadow: 0px 5px 8px 1px rgba(255,255,255,0.79);">`;
+  }, 1000);
 
-}
+  var response = await fetch(
+    `api.openweathermap.org/data/2.5/weather?lat=${a}&lon=${b}&appid=666a220a026f834ea81b0fc12f61a26f`
+  );
+  var data = response.json();
+  console.log(data);
+};
 
+document.getElementById("compress").addEventListener("click", () => {
+  document.getElementById("list").style.display = "none";
+  document.getElementById("map").style.width = "100%";
+  document.getElementById("left-scroll").style.display = "none";
+  document.getElementById("right-scroll").style.display = "none";
+});
+document.getElementById("expand").addEventListener("click", () => {
+  document.getElementById("list").style.display = "flex";
+  document.getElementById("map").style.width = "100vw";
+  document.getElementById("left-scroll").style.display = "";
+  document.getElementById("right-scroll").style.display = "";
+});
 
+const slide = (direction) => {
+  var container = document.getElementById("list");
+  scrollCompleted = 0;
+  var slideVar = setInterval(function () {
+    if (direction == "left") {
+      container.scrollLeft -= 10;
+    } else {
+      container.scrollLeft += 10;
+    }
+    scrollCompleted += 10;
+    if (scrollCompleted >= 100) {
+      window.clearInterval(slideVar);
+    }
+  }, 10);
+};
+
+document.getElementById("left-scroll").addEventListener("click", (event) => {
+  slide("left");
+});
+document.getElementById("right-scroll").addEventListener("click", (event) => {
+  slide("right");
+});
--- data.js
+++ data.js
@@ -7,6 +7,7 @@ const tracksData=[{
     "properties": {
       "name": "Autodromo Monza",
       "imageUrl":"./trackPopups/monza.jpeg",
+      "thumbnail":"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1076/articles/2017/03/race-course-monza-italy-1518567393.jpg?crop=1xw:0.786xh;center,top&resize=1200:*",
       "videoUrl":'./monza.mp4'
     }
   },
@@ -19,6 +20,7 @@ const tracksData=[{
     "properties": {
       "name": "Spa-Francorchamps",
       "imageUrl":"./trackPopups/spa.jpeg",
+      "thumbnail":"https://d3cm515ijfiu6w.cloudfront.net/wp-content/uploads/2021/08/27160645/lewis-hamilton-enters-eau-rouge-2021-planetf1.jpg",
       "videoUrl":'./spa.mp4',
     }
   },
@@ -26,81 +28,54 @@ const tracksData=[{
     "type": "Feature",
     "geometry": {
       "type": "Point",
-      "coordinates": [11.7144,44.3441]
+      "coordinates": [1.0147,52.0733]
     },
     "properties": {
-      "name": "Imoladrome",
-      "imageUrl":"./trackPopups/imola.jpeg"
+      "name": "Silverstone Circuit",
+      "imageUrl":"./trackPopups/spa.jpeg",
+      "thumbnail":"https://populous.com/wp-content/uploads/2018/01/POP_07_3037_00_3037_Silverstone_Exterior_Day1_DarrenHeath_FullRights.jpg",
+      "videoUrl":'./spa.mp4',
     }
   },
   {
     "type": "Feature",
     "geometry": {
       "type": "Point",
-      "coordinates": [50.5113,26.0316]
+      "coordinates": [19.2506,47.5817]
     },
     "properties": {
-      "name": "Bahrain",
-      "imageUrl":"./trackPopups/bahrain.jpeg"
+      "name": "Hungaroring",
+      "imageUrl":"./trackPopups/spa.jpeg",
+      "thumbnail":"https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Hungary.jpg.transform/9col/image.jpg",
+      "videoUrl":'./spa.mp4',
     }
   },
   {
     "type": "Feature",
     "geometry": {
       "type": "Point",
-      "coordinates": [-1.0168,52.0738]
+      "coordinates": [43.4057,39.9578]
     },
     "properties": {
-      "name": "Silverstone",
-      "imageUrl":"./trackPopups/silverstone.jpeg",
-      "videoUrl":'./silverstone.mp4',
+      "name": "Sochi",
+      "imageUrl":"./trackPopups/spa.jpeg",
+      "thumbnail":"https://d3cm515ijfiu6w.cloudfront.net/wp-content/uploads/2021/08/27160645/lewis-hamilton-enters-eau-rouge-2021-planetf1.jpg",
+      "videoUrl":'./spa.mp4',
     }
   },
   {
     "type": "Feature",
     "geometry": {
       "type": "Point",
-      "coordinates": [7.4204,43.7348]
+      "coordinates": [5.9720,50.4369]
     },
     "properties": {
-      "name": "Monaco",
-      "imageUrl":"./trackPopups/silverstone.jpeg",
-      "videoUrl":'./monaco.mp4'
-
+      "name": "Spa-Francorchamps",
+      "imageUrl":"./trackPopups/spa.jpeg",
+      "thumbnail":"https://d3cm515ijfiu6w.cloudfront.net/wp-content/uploads/2021/08/27160645/lewis-hamilton-enters-eau-rouge-2021-planetf1.jpg",
+      "videoUrl":'./spa.mp4',
     }
   },
-  {
-    "type": "Feature",
-    "geometry": {
-      "type": "Point",
-      "coordinates": [49.8532,40.3729]
-    },
-    "properties": {
-      "name": "Baku City Circuit",
-      "imageUrl":"./trackPopups/silverstone.jpeg",
-      "videoUrl":'./baku.mp4'
-    }
-  },
-  {
-    "type": "Feature",
-    "geometry": {
-      "type": "Point",
-      "coordinates": [5.7883,43.2490]
-    },
-    "properties": {
-      "name": "Circuit  Paul Ricard",
-      "imageUrl":"./trackPopups/silverstone.jpeg"
-    }
-  },
-  {
-    "type": "Feature",
-    "geometry": {
-      "type": "Point",
-      "coordinates": [14.7652,47.2202]
-    },
-    "properties": {
-      "name": "Red Bull Ring",
-      "imageUrl":"./trackPopups/silverstone.jpeg"
-    }
-  },
+  
+  
 ]
